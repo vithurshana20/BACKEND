1595cbfb73f7b76562721dbf7130b66701bea904
vithurshana20 <vithurz208@gmail.com>
2025-06-06 12:31:28 +0530

    mongo

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 import app from './app.js';
-import mongoose from 'mongoose';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
 
-connectDB();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT;
+const startServer = async () => {
+  await connectDB();  // wait till DB connected
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+};
 
-
-mongoose.connect(process.env.mongodb_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-    // Start server only after DB connection
-    app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}`);
-    });
-}).catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+startServer();
